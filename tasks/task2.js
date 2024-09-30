5518f9e3ec106a3e720491e8afe1d8a0f489a90f
AiwaMD <141828804+AiwaMD@users.noreply.github.com>
2024-09-30 17:01:51 +0300

    Update task2.js

--- tasks/task2.js
+++ tasks/task2.js
@@ -1,11 +1,15 @@
-// Задача: Написати функцію, яка приймає рядок і повертає його у зворотному порядку,
-//  при цьому пропускаючи всі цифри.
-
 function reverseWithoutNumbers(str) {
-  // Ваш код тут
+  let result = '';
+  
+  for (let i = str.length - 1; i >= 0; i--) {
+    if (isNaN(str[i]) || str[i] === ' ') { 
+      result += str[i];
+    }
+  }
+  return result;
 }
 
-console.log(reverseWithoutNumbers("hello123world456")); // Виведе: "dlrowolleh"
-console.log(reverseWithoutNumbers("abc123xyz"));       // Виведе: "zyxabc"
+console.log(reverseWithoutNumbers("hello123world456")); // "dlrowolleh"
+console.log(reverseWithoutNumbers("abc123xyz"));        // "zyxabc"
 
 module.exports = reverseWithoutNumbers;
