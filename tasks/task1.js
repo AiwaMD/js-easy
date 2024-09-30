5a7423a43f2b3a441f458426da7c61d94d0cb542
AiwaMD <141828804+AiwaMD@users.noreply.github.com>
2024-09-30 17:00:21 +0300

    Update task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,11 +1,18 @@
-// Задача: Написати функцію, яка приймає рядок і замінює всі голосні (a, e, i, o, u) 
-// на певний символ, наприклад *.
-
 function replaceVowels(str) {
-  // Ваш код тут
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  let result = '';
+  
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i].toLowerCase())) {
+      result += '*';
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
 }
 
-console.log(replaceVowels("hello world")); // Виведе: "h*ll* w*rld"
-console.log(replaceVowels("Javascript"));  // Виведе: "J*v*scr*pt"
+console.log(replaceVowels("hello world")); // "h*ll* w*rld"
+console.log(replaceVowels("Javascript"));  // "J*v*scr*pt"
 
 module.exports = replaceVowels;
